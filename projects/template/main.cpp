2b44971954db6609f7a0528d50b8e542f5a6085c
mrclputra <marcelino.ares.putra@gmail.com>
2026-06-05 14:32:07 +0800

    logger

--- projects/template/main.cpp
+++ projects/template/main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include <windows.h>
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    spdlog::set_pattern("[%H:%M:%S] [%^%l%$] [%s:%#] %v");
+
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+
+    SPDLOG_INFO("HELLO WORLD");
+    SPDLOG_INFO("spdlog v{}.{}", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR);
+    SPDLOG_INFO("");
+    SPDLOG_INFO("cores {}", si.dwNumberOfProcessors);
+    SPDLOG_INFO("page size {} bytes", si.dwPageSize);
+    SPDLOG_INFO("min application address {}", si.lpMinimumApplicationAddress);
+    SPDLOG_INFO("max application address {}", si.lpMaximumApplicationAddress);
+    SPDLOG_INFO("active processor mask {}", si.dwActiveProcessorMask);
+
+
     return 0;
 }
